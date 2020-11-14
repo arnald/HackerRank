67781a4a205a9c6dfc5f53c7f31a7752a3039545
Arnald <agkiata@existanze.com>
2020-11-14 21:07:10 +0200

    Solution New Year Chaos Problem

---
+++ InterviewPreparationKit/Arrays/NewYearChaos/Solution.java
@@ -0,0 +1,50 @@
+package Arrays.NewYearChaos;
+
+import java.util.*;
+
+public class Solution {
+
+    // Complete the minimumBribes function below.
+    static void minimumBribes(int[] q) {
+        int bribes = 0;
+        for (int i = q.length - 1; i >= 0; i--) {
+            int chPos = q[i] - (i + 1);
+            if (chPos > 2) {
+                System.out.println("Too chaotic");
+                return;
+            } else {
+                int st = Math.max(0, q[i] - 2);
+                for (int j = st; j < i; j++) {
+                    if (q[j] > q[i]) bribes++;
+                }
+            }
+        }
+        System.out.println(bribes);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] q = new int[n];
+
+            String[] qItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int qItem = Integer.parseInt(qItems[i]);
+                q[i] = qItem;
+            }
+
+            minimumBribes(q);
+        }
+
+        scanner.close();
+    }
+}
